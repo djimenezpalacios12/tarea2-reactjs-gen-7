546d8054aa660f0b34f421b54ae8837e4262c594
Diego Jimenez <diego@tausistemas.cl>
2022-01-17 19:28:42 -0300

    refactor: Delete api key

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,5 +1,5 @@
 // Necesario crear cuenta en TMDb para obtener el API KEY
-const api_key = "api_key=";
+const api_key = "api_key=This is not api key";
 const base_url = "https://api.themoviedb.org/3";
 const search_url = "/discover/movie?sort_by=popularity.desc&";
 
